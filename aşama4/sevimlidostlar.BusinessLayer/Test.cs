1f146ea4c34a4a602ae2df8b8af8ac244aba185a
ismailkaracayir <83426543+ismailkaracayir@users.noreply.github.com>
2021-12-25 11:28:51 +0300

    aşama2

---
+++ aşama4/sevimlidostlar.BusinessLayer/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sevimlidostlar.BusinessLayer
+{
+   public class Test
+    {
+        public Test()
+        {
+            dataAccessLayer.DatabaseContext db = new dataAccessLayer.DatabaseContext();
+            db.Database.CreateIfNotExists();
+        }
+    }
+}
